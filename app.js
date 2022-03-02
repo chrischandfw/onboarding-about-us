49938e87644ab98b700b4165bc908764ef0be581
Chris_Chan <chrischandfw@gmail.com>
2022-03-02 17:12:06 -0600

    imported data and created DOM elements

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 // import functions
+import { people } from './data.js';
 
+console.log(people, 'people')
 // grab DOM elements
+const selectElement = document.querySelector('.select');
+
+const divContainer = document.querySelector('.contain');
+const name = document.getElementById('name');
+
+const pronouns = document.querySelector('.pronouns');
+const favPlace = document.querySelector('.favplace'); 
+const hobbies = document.querySelector('.hobbies'); 
+
+console.log(hobbies, 'hobbies');
+
 
 // set event listeners 
     // get user input
