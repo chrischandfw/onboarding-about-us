4e821bb58f4d445061cd626fedf1391b69bbc842
Chris_Chan <chrischandfw@gmail.com>
2022-03-03 14:35:13 -0600

    fixed syntax error on app.js file

--- app.js
+++ app.js
@@ -6,40 +6,39 @@ import { people } from './data.js';
 const selectElement = document.querySelector('.select');
 
 const divContainer = document.querySelector('.contain');
-const name = document.getElementById('.name');
+const name = document.getElementById('name');
 
 const pronouns = document.querySelector('.pronouns');
 const favPlace = document.querySelector('.favPlace');
 const hobbies = document.querySelector('.hobbies');
 
 
-
 // set event listeners 
 selectElement.addEventListener('change', (e) => {
-    const selected = e.target.value;
-    hobbies.innerHTML = ' ';
-    divContainer.classList.remove('Delaney', 'Chris');
-    if (selected === 'one') {
-        renderPerson(0);
-    } else {
-        renderPerson(1);
-    }
+	const selected = e.target.value;
+	hobbies.innerHTML = ' ';
+	divContainer.classList.remove('Delaney', 'Chris');
+	if (selected === 'one') {
+		renderPerson(0);
+	} else {
+		renderPerson(1);
+	}
 });
 function renderPerson(index) {
-    divContainer.classList.add(`${people[index].name}`);
-    name.textContent = 'Name: ' + people[index].name;
-    pronouns.textContent = 'Pronouns: ' + people[index].pronouns;
-    favPlace.textContent = 'Favorite Place: ' + people[index].favorite_place;
+	divContainer.classList.add(`${people[index].name}`);
+	name.textContent = 'Name: ' + people[index].name;
+	pronouns.textContent = 'Pronouns: ' + people[index].pronouns;
+	favPlace.textContent = 'Favorite Place: ' + people[index].favorite_place;
 
-    for (let hobby of people[index].hobbies) {
-        const li = document.createElement('li');
-        li.textContent = hobby;
-        hobbies.appendChild(li);
-    }
+	for (let hobby of people[index].hobbies) {
+		const li = document.createElement('li');
+		li.textContent = hobby;
+		hobbies.appendChild(li);
+	}
 
 
 
-    // get user input
-    // use user input to update state 
-    // update DOM to reflect the new state
+	// get user input
+	// use user input to update state 
+	// update DOM to reflect the new state
 }
